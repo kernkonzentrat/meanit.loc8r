6dc0f68abdd560103207204463a2c550c906a50d
rrush <rrush@sapient.com>
2014-07-23 14:51:46 +0200

    restructured views folder put content data into cotrollers updated views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -6,16 +6,105 @@
  */
 
 // GET 'Location List (Home)' page
-exports.locationList = function(req, res){
-    res.render('locations-list', { title: 'Home' });
+exports.locationList = function ( req, res ) {
+    res.render('locations/locations-list', {
+        title: 'MeanIt Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'MeanIt Loc8r',
+            strapline: 'Find a place to work with wifi near you'
+        },
+        sidebar: 'Looking for wifi and a seat? MeanIt Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or pint? Let MeanIt Loc8r help to find the place you are looking for.',
+        locations: [
+            {
+                name: 'Starcups',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 3,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '100m'
+            },
+            {
+                name: 'Cafe Hero',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 5,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '150m'
+            },
+            {
+                name: 'Burger Queen',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 4,
+                facilities: ['Food', 'Premium wifi'],
+                distance: '250m'
+            }
+        ]
+    });
 };
 
 // GET 'Location info' page
-exports.locationInfo = function(req, res){
-    res.render('location-info', { title: 'Location info' });
+exports.locationInfo = function ( req, res ) {
+    res.render('locations/location-info', {
+        title: 'Starcups',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        sidebar: {
+            context: 'is on Loc8r beacuse it has accessible wifi and space to sit down with your laptop and get some work done.',
+            cTA: 'If you\'ve been here and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Starcups',
+            address: 'Münchener Strasse 37b, 86415 Mering',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {
+                lat: 48.26350551,
+                lng: 10.98502994
+            },
+            openingTimes: [
+                {
+                    days: 'Monday - Friday',
+                    opening: '7:00am',
+                    closing: '7:00pm',
+                    closed: false
+                },
+                {
+                    days: 'Saturday',
+                    opening: '8:00am',
+                    closing: '5:00pm',
+                    closed: false
+                },
+                {
+                    days: 'Sunday',
+                    closed: true
+                }
+            ],
+            reviews: [
+                {
+                    author: 'Robin Rush',
+                    rating: 5,
+                    timestamp: '16 July 2014',
+                    reviewText: 'A great place. Ultra fast wifi connection and cold beer. Extremely speeded up my work.'
+                },
+                {
+                    author: 'Kurt Knauser',
+                    rating: 3,
+                    timestamp: '17 July 2014',
+                    reviewText: 'Location is ok. But there was a drunk guy next to me, that really pissed me off.'
+                }
+            ]
+        }
+    });
 };
 
 // GET 'Add review' page
-exports.addReview = function(req, res){
-    res.render('location-review-form', { title: 'Add review' });
+exports.addReview = function ( req, res ) {
+    res.render('locations/location-review-form', {
+        title: 'Review Starcups on MeanIt Loc8r',
+        pageHeader: {
+            title: 'Review Starcups'
+        },
+        user: {
+            displayName: 'Robin Rush'
+        }
+    });
 };
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,16 +1,19 @@
 /*jslint forin: true, sloppy: true, unparam: true, vars: true, white: true, nomen: true, plusplus:true */
-/*global window, document, jQuery, console, exports */
+/*global window, document, jQuery, console, exports, res */
 
 /**
  * Controller for collection "others"
  */
 
-// GET 'About us' page
-exports.about = function(req, res){
-    res.render('generic-text', { title: 'About' });
+// GET 'About us' page exports.about = function ( req, res ) {
+exports.about = function ( req, res ) {
+    res.render('generic/generic-text', {
+        title: 'About MeanIt Loc8r',
+        content: 'Under the bed leave dead animals as gifts. Cat snacks intently sniff hand need to chase tail behind the couch. Mark territory nap all day make muffins attack feet. Make muffins throwup on your pillow so shake treat bag, yet give attitude. Stare at ceiling intently stare at the same spot for need to chase tail need to chase tail chase imaginary bugs run in circles, so give attitude. Stare at ceiling roll around missing until dinner time, stick butt in face. Chase mice give attitude yet under the bed run in circles. Lick butt intrigued by the shower, swat at dog for find something else more interesting so give attitude but intently sniff hand. Sleep on keyboard intently sniff hand and sun bathe chase imaginary bugs. Swat at dog hunt anything that moves play time so intrigued by the shower.\n\n Chase mice stretch but chase imaginary bugs, chase imaginary bugs flop over rub face on everything. Rub face on everything roll around leave dead animals as gifts claw drapes but stretch behind the couch climb leg. Roll around destroy couch under the bed but rub face on everything and mark territory or why must they do that. Nap all day why must they do that or climb leg intently sniff hand find something else more interesting. Intently stare at the same spot under the bed. Inspect anything brought into the house destroy couch and intently stare at the same spot leave hair everywhere destroy couch for throwup on your pillow.'
+    });
 };
 
 // GET 'Sign in' page
-exports.signin = function(req, res){
-    res.render('signin', { title: 'Sign in to MeanIt Loc8r' });
+exports.signin = function ( req, res ) {
+    res.render('main/signin', { title: 'Sign in to MeanIt Loc8r' });
 };
